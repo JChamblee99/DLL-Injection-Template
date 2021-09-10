1f8e695c3bb795ba79e174f9ca4d92fcb9ef8b18
JChamblee99 <jtc38@students.uwf.edu>
2021-09-10 11:49:30 -0500

    Added dynamic pointer search to template

--- dllmain.cpp
+++ dllmain.cpp
@@ -1,5 +1,25 @@
 #include <windows.h>
 
+struct memory_ptr {
+	char* moduleName;
+	uintptr_t moduleOffset;
+	int offsetCount;
+	uintptr_t offsets[];
+};
+
+void* GetDynamicPtr(HANDLE hProc, memory_ptr* ptr) {
+	uintptr_t moduleBaseAddr = (uintptr_t)GetModuleHandle(ptr->moduleName);
+	uintptr_t dynPtrBaseAddr = moduleBaseAddr + ptr->moduleOffset;
+
+	uintptr_t dynPtr = dynPtrBaseAddr;
+	for(int i = 0; i < ptr->offsetCount; ++i) {
+		ReadProcessMemory(hProc, (void*)dynPtr, &dynPtr, sizeof(dynPtr), 0);
+		dynPtr += ptr->offsets[i];
+	}
+
+	return (void*)dynPtr;
+}
+
 DWORD WINAPI MainThread(LPVOID param) {
 	while (true) {
 		if (GetAsyncKeyState(VK_F6) & 0x80000) {
